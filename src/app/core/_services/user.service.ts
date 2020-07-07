fa279edbc0053df816da79f081dc2d58baf5997c
Claudio Junior Familia Morel <cfamilia@mardom.com>
2020-07-07 17:05:22 -0400

    Can look for users just with name o lastname

--- src/app/core/_services/user.service.ts
+++ src/app/core/_services/user.service.ts
@@ -15,15 +15,8 @@ export class UserService extends BaseService<user, number> {
     return this._httpClient.get<any>(endpoint.userUrl, {headers: this.jwt()});
   }
 
-  validateUser(email: string) {
-    let index = email.indexOf('@');
-
-    if(index == -1){
-      return this._httpClient.get<any>(`http://192.168.50.5:7001/api/sistemas/empleados?pagesize=10&pagenumber=1?&nombre=${email}`);
-    }
-
-    const userName = email.substr(0, index);
-    return this._httpClient.get<any>(`http://192.168.50.5:7001/api/sistemas/empleados?pagesize=10&pagenumber=1?&username=${userName}`);
+  validateUser(email: string) {        
+    return this._httpClient.get<any>(`http://192.168.50.5:7001/api/sistemas/empleados?pagesize=3&pagenumber=1&nombre=%27${email}`);    
   }
 
   createUser(user: user){
