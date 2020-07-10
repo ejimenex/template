daed8be115fd2c90211bff2fa81c4d0f668ad52a
Claudio Junior Familia Morel <cfamilia@mardom.com>
2020-07-10 15:09:05 -0400

    Correcion de bugs acordados con QA

--- src/app/core/_services/user.service.ts
+++ src/app/core/_services/user.service.ts
@@ -15,8 +15,12 @@ export class UserService extends BaseService<user, number> {
     return this._httpClient.get<any>(endpoint.userUrl, {headers: this.jwt()});
   }
 
-  validateUser(email: string) {        
-    return this._httpClient.get<any>(`http://192.168.50.5:7001/api/sistemas/empleados?pagesize=3&pagenumber=1&nombre=%27${email}`);    
+  validateUser(email: string) {
+    if(email.indexOf('@') != -1){
+      return this._httpClient.get<any>(`http://192.168.50.5:7001/api/sistemas/empleados?pagesize=3&pagenumber=1&email=${email}`);    
+    }else{
+      return this._httpClient.get<any>(`http://192.168.50.5:7001/api/sistemas/empleados?pagesize=3&pagenumber=1&nombre=%27${email}`);    
+    }
   }
 
   createUser(user: user){
